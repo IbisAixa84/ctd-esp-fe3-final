d4f2db4bb7669d6d4249fd6b0eaf095826574fd8
Ibis Fortunato <ibisfortunato@gmail.com>
2023-04-02 03:11:19 -0300

    lista de odontologos y guardado en favoritos

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,37 @@ import Detail from "./Routes/Detail"
 import Favs from "./Routes/Favs"
 import Footer from "./Components/Footer"
 import Navbar from "./Components/Navbar"
+import React, {useState} from 'react'
 import {ThemeContext} from "./Components/ThemeContext"
 import "./App.css"
 import Card from './Components/Card'
-
 
 function App() {
   
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
+
+  //Hacer un llamado a la API para traerte la lista de odontologos
+  /* 
+  const odontologos = fetch(obtenes la lista de odontologos)
+  reemplazas lo de abajo por => const [odontologos, guardarOdontologo] = useState(odontologos)
+  */
+
+
+  const [odontologos, guardarOdontologo] = useState([
+    { id: 1, name: 'Guadalupe Sayago', username: 'Lupe2011'},
+    { id: 2, name: 'Ibis Fortunato', username: 'Ibis1984'},
+    { id: 3, name: 'Pablo Sayago', username: 'Pablo1983'},
+    { id: 4, name: 'Lautaro Diosquez', username: 'Lauta2004'}
+  ])
+
+  let favoritos = localStorage.getItem('favoritos');
+
+  const listaFavoritos = favoritos==='' || favoritos == undefined ? [] : JSON.parse(favoritos);
+
+
+
+  const [odontologosFav, agregarOdontologoFav] = useState(listaFavoritos)
 
   return (
      
@@ -27,11 +49,20 @@ function App() {
               <Route path='/detail/:id' element={<Detail/>}/>
               <Route path='/favs/' element={<Favs/>}/>
           </Routes>
-        <Card/>
+        <h1>Lista de Odontologos</h1>
+        {odontologos.map(odontologo =>(
+          <Card
+            key={odontologo.id}
+            name={odontologo.name}
+            username={odontologo.username}
+            id={odontologo.id}
+            odontologosFav={odontologosFav}
+            agregarOdontologoFav={agregarOdontologoFav}
+            odontologos={odontologos}
+          />
+        ))}
         <Footer/> 
       </div>
-    
-      
   )
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 
 
-const Card = ({ name, username, id }) => {
+const Card = ({ name, username, id, odontologosFav, agregarOdontologoFav, odontologos}) => {
 
-  const addFav = ()=>{
+  const miStorage = window.localStorage;
+
+  const addFav = id => {
     // Aqui iria la logica para agregar la Card en el localStorage
+
+    console.log(odontologosFav);
+  
+    const odontologo = odontologos.filter(odontologo => odontologo.id === id)[0];
+
+    if (!odontologosFav.some( element => element.id === id)){
+      agregarOdontologoFav([
+        ...odontologosFav,
+        odontologo
+      ])
+    }
+
+    localStorage.setItem('favoritos', JSON.stringify(odontologosFav));
+    
   }
 
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
-
+           {name}<br></br>{username}<br></br>ID = {id} 
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+        <button onClick={ () => addFav(id)} className="favButton">Add fav</button>
     </div>
   );
 };
