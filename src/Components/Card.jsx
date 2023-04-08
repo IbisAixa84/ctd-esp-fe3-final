234ff1aca16c305125b6e2e25778033b6d74425b
Ibis Fortunato <ibisfortunato@gmail.com>
2023-04-08 04:51:32 -0300

    Arreglo clases de CSS para dark-mode

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,6 +6,9 @@ import { useGlobalState } from "./utils/Context";
 
 const Card = ({ name, username, id}) => {
 
+  const {state} = useGlobalState()
+  const darkMode = state.darkMode
+
   const {odontologos, odontologosFav, agregarOdontologoFav} = useGlobalState()
   
 
@@ -31,7 +34,7 @@ const Card = ({ name, username, id}) => {
         {/* En cada card deberan mostrar en name - username y el id */}
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
         <img src='/images/doctor.jpg' alt='DH-logo'  width={'90%'} height={'90%'}/>
-        <Link key={id} to={routes.detail + '/' + id} >{name}</Link>
+        <Link className={`navi ${darkMode ? "bg-dark-nav" : "bg-light"}`} key={id} to={routes.detail + '/' + id} >{name}</Link>
         <br></br>{username}<br></br>ID = {id} 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
         <button onClick={ () => addFav(id)} className="favButton"><img src={window.location.origin + '/images/estrella.png' }  alt='DH-logo' width={'10%'} /></button>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 import ThemeButton from "./ThemeButton"
 import { Link } from 'react-router-dom'
 import { routes} from '../routes'
+import { useGlobalState } from './utils/Context'
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
 
+  const {state} = useGlobalState()
+  const darkMode = state.darkMode
+
   return (
-    <nav>
+    <nav className={`navi ${darkMode ? "bg-dark-nav" : "bg-light"}`}>
       <h2>DH Odonto</h2>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-        <Link to={routes.home}><h4>Home</h4></Link>
-        <Link to={routes.contact}><h4>Contact</h4></Link>
-        <Link to={routes.favs}><h4>Favs</h4></Link>
+      <div className={`navi ${darkMode ? "bg-dark-nav" : "bg-light"}`}>
+        <Link className={`navi ${darkMode ? "bg-dark-nav" : "bg-light"}`} to={routes.home}><h4>Home</h4></Link>
+        <Link className={`navi ${darkMode ? "bg-dark-nav" : "bg-light"}`} to={routes.contact}><h4>Contact</h4></Link>
+        <Link className={`navi ${darkMode ? "bg-dark-nav" : "bg-light"}`} to={routes.favs}><h4>Favs</h4></Link>
+      </div>
+        
         <ThemeButton></ThemeButton>
     </nav>
   )
